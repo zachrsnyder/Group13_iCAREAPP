ff253e252a31c4473cc63355c390be3122b098b8
zachrsnyder <zachrsnyder2004@gmail.com>
2024-10-23 17:18:41 -0500

    Added react frontend and scripts to bundle with webpack, as well as instructions on how to bundle to the .NET project when building for communication with backend

---
+++ client/README.md
@@ -0,0 +1,3 @@
+<b>Development:</b> Webpack Dev Server runs with npm start to serve files from memory (using devServer settings).
+
+<b>Production:</b> You bundle the React app using npm run build, which writes the output to the wwwroot folder (no devServer is needed in this scenario).
---
+++ client/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  // Entry point for your React app or JavaScript code
+  entry: './src/index.js', // Change this to where your React components start
+  output: {
+    //TODO: to run with backend, place the bundle.js file in 'wwwroot/dist' !
+    path: path.resolve(__dirname, 'dist'), // Where the bundled files will be saved
+    filename: 'bundle.js', // Output file name
+    publicPath: '/', // Public path for loading assets in the app
+  },
+  //TODO: comment out when adding backend!!! Thsi is purely for frontend dev.
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Serve static files
+    },
+    compress: true, // Gzip compression
+    port: 3000, // Port number for dev server (can be changed)
+    hot: true, // Enable hot module reloading
+    historyApiFallback: true, // Fallback to index.html for SPA routing
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // Handle JavaScript/React files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/, // Handle CSS imports
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Auto-resolve file extensions
+  },
+  mode: 'development', // Switch to 'production' for production builds
+};
+
+
