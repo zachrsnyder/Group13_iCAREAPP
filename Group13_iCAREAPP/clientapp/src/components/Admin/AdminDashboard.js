02b9cf9ff830943a0930e91e5620cc2365b67285
aaron-yang799 <aaron.yang799@gmail.com>
2024-10-25 16:45:41 -0500

    Made it so that admin email input box when creating a user only appears when admin is selected

--- Group13_iCAREAPP/clientapp/src/components/Admin/AdminDashboard.js
+++ Group13_iCAREAPP/clientapp/src/components/Admin/AdminDashboard.js
@@ -262,14 +262,18 @@ export default function AdminDashboard() {
                                     />
                                 </div>
                                 <div>
-                                    <label className="block text-sm font-medium text-gray-700">Admin Email</label>
-                                    <input
-                                        type="email"
-                                        value={newUser.adminEmail}
-                                        onChange={(e) => setNewUser({ ...newUser, adminEmail: e.target.value })}
-                                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-                                        required
-                                    />
+                                    {(newUser.roleID === '1') && (
+                                        <>
+                                            <label className="block text-sm font-medium text-gray-700">Admin Email</label>
+                                            <input
+                                                type="email"
+                                                value={newUser.adminEmail}
+                                                onChange={(e) => setNewUser({ ...newUser, adminEmail: e.target.value })}
+                                                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                                                required
+                                            />
+                                        </>
+                                    )}
                                 </div>
                                 <div>
                                     <label className="block text-sm font-medium text-gray-700">Role</label>
