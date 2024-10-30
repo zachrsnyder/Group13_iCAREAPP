86e2d041bb43d5b12ab714c4db4773d5f526e4aa
aaron-yang799 <aaron.yang799@gmail.com>
2024-10-30 18:42:24 -0500

    Changed styling for drug modal and add document

--- Group13_iCAREAPP/clientapp/src/components/Worker/MyPalette/AddDocumentModal.js
+++ Group13_iCAREAPP/clientapp/src/components/Worker/MyPalette/AddDocumentModal.js
@@ -92,8 +92,8 @@ const AddDocumentModal = ({ setShowAddModal }) => {
 
     return (
         <>
-            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
-                <div className="bg-white rounded-lg shadow-xl w-full max-w-4xl h-[90vh] flex flex-col">
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50 ">
+                <div className="bg-white rounded-lg shadow-xl w-full max-w-2xl h-[90vh] flex flex-col">
                     {/* Header */}
                     <div className="flex items-center justify-between px-6 py-4 border-b">
                         <h2 className="text-xl font-semibold text-gray-800">Add New Document</h2>
